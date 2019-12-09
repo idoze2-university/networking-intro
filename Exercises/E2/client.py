da93fc08d0cdf599f7faaf4d1feb9d8bdfadd705
idoz <idoz@idoz7490-ubuntu14.checkpoint.com>
2019-12-09 18:41:33 +0200

    -

--- Exercises/E2/client.py
+++ Exercises/E2/client.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest_ip = '127.0.0.1'
+dest_ip = '192.168.1.21'
 dest_port = 12345
 s.connect((dest_ip, dest_port))
 msg = raw_input("Message to send: ")
