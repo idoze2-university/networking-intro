dd2332e98687d5b1cef8220398035dc387583869
idoz <ido.ze2@gmail.com>
2019-12-16 18:51:00 +0200

    Some work on part 2

--- Exercises/E2/client.py
+++ Exercises/E2/client.py
@@ -1,40 +1,51 @@
-import re
-from socket import socket, AF_INET, SOCK_DGRAM
+import re, glob
+import time
+from socket import socket, AF_INET, SOCK_STREAM
 import sys
 
 # Constant Variables####################
-CLIENT_MODE_LISTENING = 0  # type: int
-CLIENT_MODE_USER = 1  # type: int
+CLIENT_MODE_LISTENING = '0'  # type: int
+CLIENT_MODE_USER = '1'  # type: int
 CLIENT_MODES = [CLIENT_MODE_LISTENING, CLIENT_MODE_USER]
 DEF_CLIENT_MODE = CLIENT_MODE_USER  # type: int
 DEF_SERVER_IP = '192.168.1.21'  # type: str
 DEF_SERVER_PORT = 12345  # type: int
+DEF_LISTENING_IP = '0.0.0.0'  # type: str
+DEF_LISTENING_PORT = 12223  # type: int
+MAX_MSG_SIZE = 2048  # type: int
 ########################################
 
 if __name__ == '__main__':
-    s = socket(AF_INET, SOCK_DGRAM)
-    client_mode = (int(sys.argv[1]) if (sys.argv[1] in CLIENT_MODES) else DEF_CLIENT_MODE) if len(
-        sys.argv) > 0 else DEF_CLIENT_MODE
-    dest_ip = sys.argv[2] if len(sys.argv) > 1 else DEF_SERVER_IP
-    dest_port = int(sys.argv[3]) if len(sys.argv) > 2 else DEF_SERVER_PORT
+    s = socket(AF_INET, SOCK_STREAM)
+    client_mode = sys.argv[1] if len(sys.argv) > 1 and (sys.argv[1] in CLIENT_MODES) else DEF_CLIENT_MODE
+    dest_ip = sys.argv[2] if len(sys.argv) > 2 else DEF_SERVER_IP
+    dest_port = int(sys.argv[3]) if len(sys.argv) > 3 else DEF_SERVER_PORT
+    try:
+        s.connect((dest_ip, dest_port))
+    except:
+        print str.format("Please make sure server {0}:{1} is open.", dest_ip, dest_port)
+        exit(1)
 
-    s.bind((dest_ip, dest_port))
-    msg = raw_input("")
-    while msg != "quit":
-        if msg:
-            exp = re.search(r'\d+', msg)
-            if int(exp.group() if exp else 0) in range(1, 6):  # if msg has a number in in [1,6]
-                s.sendto(msg, (dest_ip, dest_port))
-                data, sender_info = s.recvfrom(2048)
-                try:
-                    head, body = data.split(' ', 1)
-                except:
-                    head, body = data, None
-                if head != msg[0]:
-                    print '** Some error occured on server side, please try again.'
-                if body:
-                    print body
-            else:
-                print 'illegal input'
-        msg = raw_input("")
+    if client_mode == CLIENT_MODE_LISTENING:
+        listening_port = int(sys.argv[4]) if len(sys.argv) > 4 else DEF_LISTENING_PORT
+        m = client_mode + ' '
+        m += str(listening_port) + ' '
+        m += ','.join(glob.glob1("/home/idoz/University/Networking/Exercises/E2", "*.*")) + "\n"
+        s.send(m)
+        time.sleep(1)
+        data = s.recv(MAX_MSG_SIZE)
+        print data
+        file_socket = socket(AF_INET, SOCK_STREAM)
+        file_socket.bind((DEF_LISTENING_IP, listening_port))
+        file_socket.listen(1)
+        while True:
+            continue
+
+    elif client_mode == CLIENT_MODE_USER:
+        msg = raw_input("Search: ")
+        while msg:
+            s.send(msg + "\n")
+            data = s.recvfrom(MAX_MSG_SIZE)
+            data = data.split(',')
+        msg = raw_input("Search: ")
     s.close()
--- Exercises/E2/server.py
+++ Exercises/E2/server.py
@@ -1,19 +1,77 @@
-import socket, sys
+import sys, time
+from socket import *
+from typing import List, Any
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = '0.0.0.0'
 
-server_port = sys.argv[1] if len(sys.argv) >1 else 12345
+class FileHost:
+    def __init__(self, ip, port, listening_port):
+        self.ip = ip
+        self.port = port
+        self.listening_port = listening_port
 
-server.bind((server_ip, server_port))
-server.listen(5)
-while True:
-    client_socket, client_address = server.accept()
-    print 'Connection from: ', client_address
-    data = client_socket.recv(1024)
-    while not data == '':
-        print 'Received: ', data
-        client_socket.send(data.upper())
-        data = client_socket.recv(1024)
-    print 'Client disconnected'
-    client_socket.close()
+    def __str__(self):
+        return self.ip + ':' + str(self.listening_port)
+
+    ip = ''  # type: str
+    port = 0  # type: int
+    listening_port = 0  # type: int
+
+
+class File:
+    def __init__(self, name, host):
+        self.name = name
+        self.host = host
+
+    name = ''  # type: str
+    host = None  # type: FileHost
+
+    def get_download_string(self):
+        return self.name + ' ' + str(host)
+
+
+# Constant Variables####################
+CLIENT_MODE_LISTENING = 0  # type: int
+CLIENT_MODE_USER = 1  # type: int
+CLIENT_MODES = [CLIENT_MODE_LISTENING, CLIENT_MODE_USER]
+DEF_IP = '0.0.0.0'  # type: str
+DEF_PORT = 12345  # type: int
+MAX_MSG_SIZE = 2048  # type: int
+########################################
+
+if __name__ == '__main__':
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else DEF_PORT
+    s = socket(AF_INET, SOCK_STREAM)
+    while (1):
+        try:
+            s.bind((DEF_IP, port))
+            break
+        except:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            time.sleep(0.0001)
+    print "Connected to 12345\n"
+    s.listen(10)
+
+    files = []
+    while True:
+        conn, sender = s.accept()
+        data = conn.recv(MAX_MSG_SIZE)
+        if data:
+            client_mode, body = data.split(' ', 1)
+            if int(client_mode) == CLIENT_MODE_LISTENING:
+                listening_port, file_list = body.split(' ', 1)
+                host = FileHost(sender[0], sender[1], listening_port)
+                for file_name in file_list.split(','):
+                    files.append(File(file_name, host))
+                time.sleep(1)
+                try:
+                    print 'just trying to send'
+                    s.send('1')
+                except:
+                    print 'obby it failed, nvm.'
+                    continue
+
+
+            elif int(client_mode) == CLIENT_MODE_USER:
+                continue
+
+    s.close()
